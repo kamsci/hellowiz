95a3e504f9b2504c827e4db1142dd3b4286afc85
Krista Calderon <krista@kristas1bookpro.homies.place>
2023-11-02 17:44:23 -0700

    Added SQLException catch in ExceptionManager

--- src/main/java/com/hellowiz/api/resources/middleware/CustomExceptionMapper.java
+++ src/main/java/com/hellowiz/api/resources/middleware/CustomExceptionMapper.java
@@ -3,6 +3,8 @@ package com.hellowiz.api.resources.middleware;
 import com.hellowiz.api.api.ErrorResponse;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
+import org.jdbi.v3.core.statement.UnableToExecuteStatementException;
+import org.postgresql.util.PSQLException;
 import org.slf4j.LoggerFactory;
 
 public class CustomExceptionMapper implements ExceptionMapper<Throwable> {
@@ -10,11 +12,23 @@ public class CustomExceptionMapper implements ExceptionMapper<Throwable> {
     public Response toResponse(Throwable exception) {
         // Log the exception
         logException(exception);
+        ErrorResponse errorResponse;
 
-        // You can customize the response entity and status here
-        // For example, return a JSON error response
+        if (exception instanceof UnableToExecuteStatementException) {
+            // Database error, likely a PSQLException, don't pass pull SQL statement error to client
+            if (exception.getCause() instanceof PSQLException) {
+                // Detail exposes exact SQL query, leave it out
+                String[] error = exception.getMessage().split("Detail");
+                errorResponse = new ErrorResponse(error[0]);
+            } else {
+                errorResponse = new ErrorResponse("Unable to save. Service might be unavailable or your request may have errors.");
+            }
+        } else {
+            errorResponse = new ErrorResponse(exception.getMessage());
+        }
+
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity(new ErrorResponse(exception.getMessage()))
+                .entity(errorResponse)
                 .build();
     }
 
